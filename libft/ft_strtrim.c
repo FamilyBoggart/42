356869190252804192988cbd06b457e23d7df389
Alejandro Romero Garcia <alerome2@c1r3s1.42malaga.com>
2024-05-06 13:07:51 +0200

    OK

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -5,63 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alerome2 <alerome2@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/23 12:01:26 by alerome2          #+#    #+#             */
-/*   Updated: 2024/04/23 14:03:27 by alerome2         ###   ########.fr       */
+/*   Created: 2024/05/02 17:15:20 by alerome2          #+#    #+#             */
+/*   Updated: 2024/05/06 12:51:29 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-int	check_found_char(const char *s1, const char *set, int i)
+char	*ft_strtrim(const char *s1, const char	*set)
 {
-	int	j;
-	int	is_char;
+	char	*s_aux;
+	int		len;
 
-	is_char = 0;
-	j = 0;
-	while (set[j] != '\0')
-	{
-		if (s1[i] == set[j++])
-			is_char = 1;
-	}
-	if (is_char == 0)
-		return (1);
-	else
-		return (0);
-}
-
-char	*ft_strtrim(const char *s1, const char *set)
-{
-	char	*aux;
-	int		is_char;
-	int		i;
-	int		o;
-
-	i = 0;
-	o = 0;
-	is_char = 0;
-	if (ft_strlen(set) == 0)
-		return ((char *)s1);
-	if (ft_strlen(s1) == 0)
-		return ((char *)set);
-	aux = malloc(sizeof(char) * (ft_strlen(s1) + 1));
-	while (s1[i] != '\0')
-	{
-		if (check_found_char(s1, set, i))
-			aux[o++] = s1[i];
-		i++;
-	}
-	aux[i] = '\0';
-	return (aux);
+	if (!s1 || !set)
+		return (NULL);
+	while (*s1 != '\0' && ft_strchr(set,*s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (len > 0 && ft_strchr(set, s1[len]))
+		len--;
+	s_aux = ft_substr((char *)s1, 0, len + 1);
+	return (s_aux);
 }
 /*
-#include <stdio.h>
-int main(void)
+int main (void)
 {
-	char	*s1 = "Hola mundo";
-	char 	*remover = "om";
-	printf("%s",ft_strtrim(s1,remover));
-	return 0;
+	char	*s1 = "     Hola   Mundo";
+	char	*set = " ";
+	ft_strtrim(s1,set);
 }
 */
