a9cbcc7736d2de7b5aaccf11d850df42af301fcf
FamilyBoggart <alexanderoga@gmail.com>
2024-05-08 00:04:44 +0200

    PREPARANDO UN REPOSITORIO LIMPIO
    Recientemente se han aprendido conceptos acerca de como usar git de
    manera correcta, a partir de este commit, se comenzara a realizar un
    repositorio de una forma limpia, en este primer commit en la rama master
    se han borrado algunos archivos que no estaban en su sitio y se ha
    añadido el archivo README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+Ejercicios realizados durante la etapa como estudiante
+de 42 Malaga. Cada uno de los directorios corresponden a 
+un proyecto distinto que se ha tenido que realizar
+
+Libft: Creacion de la primera libreria en lenguaje C
+Printf: Creacion de una funcion que emule el 
+    funcionamiento de la function printf
--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,48 +12,19 @@
 
 #include "libft.h"
 #include <stdlib.h>
-
-int	check_found_char(const char *s1, const char *set, int i)
-{
-	int	j;
-	int	is_char;
-
-	is_char = 0;
-	j = 0;
-	while (set[j] != '\0')
-	{
-		if (s1[i] == set[j++])
-			is_char = 1;
-	}
-	if (is_char == 0)
-		return (1);
-	else
-		return (0);
-}
+#include <stdio.h>
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
 	char	*aux;
-	int		is_char;
-	int		i;
-	int		o;
+	int	len;
 
 	i = 0;
-	o = 0;
-	is_char = 0;
-	if (ft_strlen(set) == 0)
-		return ((char *)s1);
-	if (ft_strlen(s1) == 0)
-		return ((char *)set);
-	aux = malloc(sizeof(char) * (ft_strlen(s1) + 1));
-	while (s1[i] != '\0')
-	{
-		if (check_found_char(s1, set, i))
-			aux[o++] = s1[i];
-		i++;
-	}
-	aux[i] = '\0';
-	return (aux);
+	while(*s1 && ft_strchr(set,*s1))
+		s1++;
+	len = ft_strlen(s1);
+	printf("Longitud: %d",len);
+	aux = ft_substr	
 }
 /*
 #include <stdio.h>
