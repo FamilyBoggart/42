bba8c3bf080e2805e583cc58874542bd5aecef32
alerome2 <alerome2@student.42malaga.com>
2024-05-19 09:29:41 +0200

    FT_PUTNUBR_U ADDED
    Arreglados los problemas de conversiones con unsigned int

--- printf/ft_putnbr_u.c
+++ printf/ft_putnbr_u.c
@@ -13,7 +13,57 @@
 
 #include "ft_printf.h"
 
-int ft_putnbr_u(unsigned int u_number)
+int u_n_size(unsigned int n)
 {
-	return (ft_putnbr((int)u_number));
+	int	size;
+
+	size = 0;
+	if(n == 0)
+		return (1);
+	while (n != 0)
+	{
+		n /= 10;
+		size++;
+	}
+	return (size);
+}
+char	*ft_utoa(unsigned int number)
+{
+	char 			*str;
+	unsigned int	size;
+	unsigned int	aux;
+	
+	size = u_n_size(number);
+	aux = size;
+	str = (char *)malloc(size + 1);
+	if(!str)
+		return (NULL);
+	while (size > 0)
+	{
+		str[--size] = number % 10 + '0';
+		number /= 10;
+	}
+	str[aux]='\0';
+	return (str);
+}
+
+int ft_putnbr_u(int number)
+{
+	unsigned int	u_number;
+	char			*str;
+	int				len;
+
+	if(number < 0)
+		u_number =  MAX_UINT + (number + 1);
+	else
+		u_number = number;
+	str = ft_utoa(u_number);
+	len = 0;
+	while(str[len] != '\0')
+	{
+		ft_putchar(str[len]);
+		len++;
+	}
+	free(str);
+	return (len);
 }
