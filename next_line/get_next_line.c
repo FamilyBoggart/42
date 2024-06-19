9a84002399168d14ca8bf9e6ee1511a5aff19486
alerome2 <alerome@student.42malaga.com>
2024-06-19 11:09:07 +0200

    GNL FIRST VERSION
    La funcion es capaz de leer el archivo desde get_next_line, pero no
    reconoce las lineas por salto de linea, y tampoco se bloquea al
    finalizar el archivo, sino que continúa llenando el buffer

--- next_line/get_next_line.c
+++ next_line/get_next_line.c
@@ -6,11 +6,54 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:47:22 by alerome2          #+#    #+#             */
-/*   Updated: 2024/05/28 13:51:34 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/06/19 11:07:30 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdio.h>
+
+/**
+ * @brief Get the next line object
+ * 
+ * @param fd 
+ * @return char* 
+ */
 char	*get_next_line(int fd)
 {
-	return (NULL);
+	char	*str;
+	int		br;
+
+	str = malloc (BUFFER_SIZE * sizeof(char));
+	if (!str)
+		return (NULL);
+	br = read(fd, str, BUFFER_SIZE);
+	printf("br: %d", br);
+	if (br == -1)
+		return (NULL);
+	printf("%s", str);
+	while (br != 0)
+	{
+		br = read(fd, str, BUFFER_SIZE);
+		printf("%s", str);
+	}
+	free(str);
+	return (str);
 }
+// PSEUDOCODE
+/*
+	1. EL ARCHIVO SE ABRE EN MAIN, EL FILE DESCRIPTOR SE LE PASA COMO ARGUMENTO
+	A NUESTRO GET_NEXT_LINE
+	
+	2. GNL TIENE QUE LEER EL ARCHIVO, ASI QUE HAY QUE HACER COMPROBACIONES DE
+	ERRORES
+
+	3. EL ARCHIVO DEBE LEERSE LINEA POR LINEA, ES DECIR, DEBEMOS USAR UN ARRAY
+	EN DONDE INTRODUCIR LOS DATOS ENCONTRADOS HASTA QUE ENCONTREMOS UN \n 
+
+	4. USAREMOS UNA FUNCION READER PARA LEER UN ARCHIVO HASTA ENCONTRAR \n o \0
+	
+	5. CUANDO ENCONTRAMOS UN \n LANZAMOS TODO EL BUFFER LEIDO A NUESTRA VARIABLE
+	ESTATICA EXTERNA
+	
+*/
--- next_line/get_next_line.h
+++ next_line/get_next_line.h
@@ -6,14 +6,41 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:49:50 by alerome2          #+#    #+#             */
-/*   Updated: 2024/05/28 13:51:16 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/05/29 15:56:48 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# define BUFFER_SIZE 42
 
 char	*get_next_line(int fd);
-
+/**
+ * @brief  memcpy copies the string src in the string dest with n characters
+ * 
+ * @param dest 
+ * @param src 
+ * @param n 
+ * @return void* 
+ */
+void	*ft_memcpy(void *dest, const void *src, unsigned int n);
+/**
+ * @brief strjoin concatenates string s1 to s2, being s1 the str's beggining
+ * 
+ * @param s1 
+ * @param s2 
+ * @return char* 
+ */
+char	*ft_strjoin(const char *s1, const char *s2);
+/**
+ * @brief The function returns the character c`s position in s
+ * 
+ * @param s 
+ * @param c the character which we will find in s
+ * @return char* 
+ */
+char	*ft_strchr(const char *s, int c);
 #endif
