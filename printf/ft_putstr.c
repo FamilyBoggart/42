a334df12b22f91f77c4013550151b47f7d1f89cf
Alejandro Romero Garcia <alerome2@c3r5s4.42malaga.com>
2024-06-22 21:16:58 +0200

    ADDED MAIN.C
    Fixed an error in ft_putstr.c which it should have been fixed before

--- printf/ft_putstr.c
+++ printf/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:13:31 by alerome2          #+#    #+#             */
-/*   Updated: 2024/05/23 15:28:00 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/06/22 21:15:36 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 int	ft_putstr(char *str)
 {
 	int	n;
-
+	if(!str)
+		return(ft_putstr("(null)"));
 	n = 0;
 	while (str[n] != '\0')
 		n += ft_putchar(str[n]);
