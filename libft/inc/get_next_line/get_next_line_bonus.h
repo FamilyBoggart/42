3ad321aa9fa514f493d202d407bee71356d6e703
FamilyBoggart <alexanderoga@gmail.com>
2024-07-20 13:52:24 +0200

    ADDED GNL

---
+++ libft/inc/get_next_line/get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 13:49:50 by alerome2          #+#    #+#             */
+/*   Updated: 2024/07/13 12:16:18 by alerome2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(const char *s);
+#endif
