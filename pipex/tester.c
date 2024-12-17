d7d80bef33282080847a8c67b1bc8e6ed6bcd150
FamilyBoggart <alexanderoga@gmail.com>
2024-12-17 13:54:18 +0100

    TEST 4, 5, 6 ADDED TO TESTER

--- pipex/tester.c
+++ pipex/tester.c
@@ -6,20 +6,38 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 12:45:17 by alerome2          #+#    #+#             */
-/*   Updated: 2024/12/17 13:32:42 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/12/17 13:53:52 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/pipex.h"
 
+void check(void)
+{
+	return;
+}
 int main(void)
 {
-	system ("make debug");
 	write(1, "\033[34m[1]. /pipex\n\033[0m", 22);
 	system ("./pipex");
+	check();
 	write(1, "[\033[34m2]. /pipex testfile.txt ls cat :\n\033[0m", 44);
 	system ("./pipex testfile.txt ls cat");
-	write(1, "\033[34m[3]. /pipex testfile.txt ls cat outfile.txt:\n\033[0m", 55);
+	check();
+	write(1, "\033[34m[3]. /pipex testfile1.txt ls cat outfile.txt:\n\033[0m", 56);
 	system ("./pipex testfile1.txt ls cat outfile.txt");
+	check();
+	// [4.] REPEAT COMMANDS
+	ft_printf("\033[34m[4]. /pipex testfile1.txt ls cat grep find find outfile.txt:\n\033[0m");
+	system ("./pipex testfile1.txt ls cat grep find find outfile.txt");
+	check();
+	// [5.] EVERY COMMAND IS THE SAME
+	ft_printf("\033[34m[5]. /pipex testfile1.txt ls ls ls ls ls outfile.txt:\n\033[0m");
+	system ("./pipex testfile1.txt ls ls ls ls ls outfile.txt");
+	check();
+	// [6.] ERROR. COMMAND NOT FOUND
+	ft_printf("\033[34m[6]. /pipex testfile1.txt ls cato outfile.txt:\n\033[0m");
+	system ("./pipex testfile1.txt ls cato outfile.txt");
+	check();
 	return (0);
 }
