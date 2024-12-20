194dc65dd4501d397c5c014909e7061e1b071ecd
FamilyBoggart <alexanderoga@gmail.com>
2024-12-20 15:57:49 +0100

    FIRST COMMAND IS EXECUTED

--- pipex/pipes.c
+++ pipex/pipes.c
@@ -6,7 +6,7 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 18:18:31 by alerome2          #+#    #+#             */
-/*   Updated: 2024/12/19 13:19:39 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/12/20 15:43:43 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 void pipes(void)
 {
-	pid_t	pid_C;
-	int 	fd[2]; //Fd[0] lectura. fd[1] escritura
+	pid_t	pid_c;
+	int		fd[2]; //Fd[0] lectura. fd[1] escritura
 	char	buf[10]; //Buffer de lectura
 	int		bytes_read;
 
-	if(pipe(fd) == -1)
+	if (pipe(fd) == -1)
 		ft_printf("Ha habido un error al crear el pipe\n");
-	pid_C = fork();
-	if (pid_C == -1)
+	pid_c = fork();
+	if (pid_c == -1)
 		ft_printf("Error en el fork\n");
-	else if(pid_C == 0)
+	else if( pid_c == 0)
 	{
 		close(fd[0]); //Cerramos lectura
 		write(fd[1], "abcde", 5);
 		close(fd[1]);
 		exit(0);
 	}
-	else if (pid_C > 0)
+	else if (pid_c > 0)
 	{
 		ft_printf("PROCESO PADRE: %d\n", getpid());
 		close(fd[1]); //Cerramos escritura
 		bytes_read = read(fd[0], buf, sizeof(buf));
-		ft_printf("Padre ha leido %d bytes:\t %s\n",bytes_read, buf);
+		ft_printf("Padre ha leido %d bytes:\t %s\n", bytes_read, buf);
 		close(fd[0]);
 		exit(0);
 	}
@@ -45,5 +45,19 @@ void pipes(void)
 
 void	command(t_str *args)
 {
+	int		i;
+	char	**cmd;
+
+	cmd = malloc(sizeof(char *) * 3);
+	i = 0;
 	ft_printf("Archivo a trabajar: %s\n", args->input_file);
+	while (i < args->i)
+	{
+		cmd[0] = args->cmd_path[i];
+		cmd[1] = args->input_file;
+		cmd[2] = NULL;
+		ft_printf("Comando %d:\t%s\n", i + 1, args->cmd_path[i]);
+		execv(cmd[0], cmd);
+		i++;
+	}
 }
--- pipex/utils.c
+++ pipex/utils.c
@@ -6,7 +6,7 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 13:13:35 by alerome2          #+#    #+#             */
-/*   Updated: 2024/12/17 14:12:01 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/12/20 15:30:13 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 void	ft_free(char **str)
 {
 	int	i;
-	
+
 	i = 0;
-	if(!str)
-		return;
+	if (!str)
+		return ;
 	while (str[i])
 	{
 		free(str[i]);
 		i++;
 	}
-	if(*str && str)
+	if (*str && str)
 		free(str);
 	else
 		ft_printf("ERROR DE LIBERACION DE MEMORIA\n");
 }
 
-void finish(t_str *str, int ok)
+void	finish(t_str *str, int ok)
 {
 	if (ok == 1)
 	{
