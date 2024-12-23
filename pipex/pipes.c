11f2bb22183c0df0ba9de313b3078ba8766b835b
FamilyBoggart <alexanderoga@gmail.com>
2024-12-23 18:19:19 +0100

    SIN LEAK PERO NO LEE PORQUE EL INPUT NO ES FILE

--- pipex/pipes.c
+++ pipex/pipes.c
@@ -6,7 +6,7 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 18:18:31 by alerome2          #+#    #+#             */
-/*   Updated: 2024/12/23 17:23:57 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/12/23 18:18:27 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void pipes(void)
 {
 }
 
-void	exec_process(char	**cmd, int *fd, int pid)
+char	*exec_process(char	**cmd, int *fd, int pid)
 {
 	char	*buf;
 	int		fd_aux;
@@ -35,15 +35,14 @@ void	exec_process(char	**cmd, int *fd, int pid)
 		close(fd[1]);
 		fd_aux = dup(fd[0]);
 		buf = read_output(fd_aux);
-		ft_printf("Buffer: %s\n", buf);
 		close(fd[0]);
-		free(buf);
 		free(fd);
 		free(cmd);
+		return (buf);
 	}
 }
 
-char	**preparecmd(t_str	*args)
+char	**preparecmd(t_str	*args, char *input)
 {
 	char	**cmd;
 	int		i;
@@ -51,7 +50,7 @@ char	**preparecmd(t_str	*args)
 	i = 0;
 	cmd = malloc(sizeof(char *) * 3);
 	cmd[0] = args->cmd_path[i];
-	cmd[1] = args -> input_file;
+	cmd[1] = input;
 	cmd[2] = NULL;
 	return (cmd);
 }
@@ -61,8 +60,9 @@ void	command(t_str *args)
 	char	**cmd;
 	int		*fd;
 	int		pid;
+	char	*buffer;
 
-	cmd = preparecmd(args); //Preparacion del comando
+	cmd = preparecmd(args, args->input_file); //Preparacion del comando
 	fd = malloc(sizeof(int) * 2);
 	if (!fd)
 		return ;
@@ -71,7 +71,33 @@ void	command(t_str *args)
 	pid = fork();
 	if (pid == -1)
 		return ;
-	exec_process(cmd, fd, pid);
+	buffer = exec_process(cmd, fd, pid);
+	ft_printf("Buffer:\t%s\n", buffer);
+	//free(buffer);
+	// 2ª ITERACION
+	
+	cmd = preparecmd(args, buffer); //Preparacion del comando
+	ft_printf("cmd[0]:\t%s\n", cmd[0]);
+	ft_printf("cmd[1]:\t%s\n", cmd[1]);
+	//free(cmd);
+	free(buffer);
+	fd = malloc(sizeof(int) * 2);
+	if (!fd)
+		return ;
+	if (pipe(fd) == -1)
+		return ;
+	pid = fork();
+	if (pid == -1)
+		return ;
+	//free(fd);
+	buffer = exec_process(cmd, fd, pid);
+	free(buffer);
+	/*
+	buffer = exec_process(cmd, fd, pid);
+	ft_printf("Buffer:\t%s\n", buffer);
+	free(buffer);
+	*/
+	
 	ft_printf("(command)Aqui solo llega el padre\n");
 
 }
--- pipex/tester.c
+++ pipex/tester.c
@@ -6,7 +6,7 @@
 /*   By: alerome2 <alerome2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 12:45:17 by alerome2          #+#    #+#             */
-/*   Updated: 2024/12/23 17:26:01 by alerome2         ###   ########.fr       */
+/*   Updated: 2024/12/23 17:37:02 by alerome2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,17 @@ int main(void)
 	system ("./pipex testfile1.txt ls cat grep find find outfile.txt");
 	check();
 	// [5.] EVERY COMMAND IS THE SAME
+	ft_printf("\033[31m[EVERY COMMAND IS THE SAME]\033[0m\n");
 	ft_printf("\033[34m[5]. /pipex testfile1.txt ls ls ls ls ls outfile.txt:\n\033[0m");
 	system ("./pipex testfile1.txt ls ls ls ls ls outfile.txt");
 	check();
-	// [6.] ERROR. COMMAND NOT FOUND
+
+	ft_printf("\033[31m[ERROR. COMMAND NOT FOUND]\033[0m\n");
 	ft_printf("\033[34m[6]. /pipex testfile1.txt ls cato outfile.txt:\n\033[0m");
 	system ("./pipex testfile1.txt ls cato outfile.txt");
 	check();
-	// [7.] EMPTY FILE
+	
+	ft_printf("\033[31m[EMPTY FILE]\033[0m\n");
 	ft_printf("\033[34m[7]. /pipex testfile2.txt cat ls outfile.txt:\n\033[0m");
 	system ("./pipex testfile2.txt cat ls outfile.txt");
 	check();
